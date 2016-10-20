0bb0c5442dcd329660c263c836e9c1e4017dbade
Alejandro Brunacci <Abrunacci@bitson.com.ar>
2016-10-20 18:49:49 -0300

    flip interactive

--- plugins/flip.py
+++ plugins/flip.py
@@ -17,12 +17,28 @@ def flip(message):
         chat_id = str(message.chat.id)
         args = get_content(message)
         coin = ['cara', 'seca']
-        if args != '-?':
+        if not args:
+            bot.send_message(chat_id,
+                             'Ingrese su elección... \n '
+                             '¿cara o seca? \n'
+                             '/flip <eleccion>',
+                             parse_mode='Markdown')
+            return
+        if args != '-?' and args in coin:
+            coin = random.choice(coin)
+            if args == coin:
                 bot.send_message(chat_id,
-                                 random.choice(coin),
+                                 'Salio %s. Ganaste.' % coin,
+                                 parse_mode="Markdown")
+            else:
+                bot.send_message(chat_id,
+                                 'Salio %s. Perdiste.' % coin,
                                  parse_mode="Markdown")
         elif args == '-?':
             bot.send_message(chat_id,
-                             'Tira la moneda... <Próximamente interactivo!>')
+                             'Tira la moneda...  /flip <cara/seca>',
+                             parse_mode='Markdown')
         else:
-            pass
+            bot.send_message(chat_id,
+                             'La opción elegida es inválida.',
+                             parse_mode='Markdown')
